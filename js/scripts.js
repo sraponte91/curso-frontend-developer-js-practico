d9c71e416e53bb27de6ca1e3b58391c4a414abd9
Beauty Depot <beautydepot@Beautys-MacBook-Pro.local>
2022-10-24 17:12:49 -0400

    class-19-finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector ('.desktop-menu');
+const burgerMobile = document.querySelector ('.menu');
+const mobileMenu = document.querySelector ('.mobile-menu');
+
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+burgerMobile.addEventListener ('click', toggleMobileMenu);
 
+//Functions for the desktopMenu togglingAction
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
+}
+
+//Funtions for the mobile menu togglingAction
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
 }
