7e2b735ac23b53fc662a0efe2247a85e67d3cd86
Beauty Depot <beautydepot@Beautys-MacBook-Pro.local>
2022-10-27 12:21:56 -0400

    ending-of-course

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,25 @@ const mobileMenu = document.querySelector ('.mobile-menu');
 const btnCart = document.querySelector ('.navbar-shopping-cart');
 const cart = document.querySelector ('#shoppingCartContainer');
 const cartContainer = document.querySelector ('.cards-container');
+const prdDtlCont = document.querySelector ('#productDetail');
+const closePrdDtlCont = document.querySelector ('.product-detail-close');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 burgerMobile.addEventListener ('click', toggleMobileMenu);
 btnCart.addEventListener ('click', toggleProductDetail);
+closePrdDtlCont.addEventListener('click', closeTheDetails);
 
 //Functions for the desktopMenu togglingAction
 function toggleDesktopMenu () {
     const isCartClosed = cart.classList.contains('inactive');
+    const isDetailsClosed = prdDtlCont.classList.contains('inactive');
     if (!isCartClosed) {
         cart.classList.add('inactive');
+    } if (!isDetailsClosed) {
+        prdDtlCont.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
-
 }
 
 //Funtions for the mobile menu togglingAction
@@ -29,7 +34,6 @@ function toggleMobileMenu () {
     if (!isCartClosed) {
         cart.classList.add('inactive');
     }
-
     mobileMenu.classList.toggle('inactive');
 }
 
@@ -43,7 +47,20 @@ function toggleProductDetail () {
     } if (!isDesktopMenuClosed) {
         desktopMenu.classList.add('inactive');
     }
+
     cart.classList.toggle('inactive');
+}
+
+function openTheDetails () {
+    desktopMenu.classList.add('inactive');
+    cart.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+
+    prdDtlCont.classList.remove('inactive');
+}
+
+function closeTheDetails () {
+    prdDtlCont.classList.add('inactive');
 }
 
 // Productos
@@ -63,6 +80,7 @@ productList.push({
     precio: '320',
     img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
+
 function renderProducts (array) {
     for (product of array) {
         const productCard = document.createElement('div');
@@ -70,6 +88,7 @@ function renderProducts (array) {
 
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.img);
+        productImg.addEventListener('click', openTheDetails)
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -93,8 +112,6 @@ function renderProducts (array) {
         productCard.append(productImg, productInfo);
 
         cartContainer.appendChild(productCard);
-
-        
     };
 }
 
